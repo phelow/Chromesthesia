2a7a0ba627105e9d341089f78cd7bca9dc5aee39
phelow <wpheloung@gmail.com>
2016-11-26 17:43:13 -0500

    Level 2 on new system

--- Assets/SliderBox.cs
+++ Assets/SliderBox.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class SliderBox : MonoBehaviour {
+    [SerializeField]
+    protected GameObject m_leftTerminus;
+    [SerializeField]
+    protected GameObject m_rightTerminus;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    protected float m_minValue;
+    [SerializeField]
+    protected float m_maxValue;
+    
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        SetValueInHeavy(Mathf.Lerp(m_minValue, m_maxValue, Mathf.InverseLerp(m_leftTerminus.transform.position.x, m_rightTerminus.transform.position.x, this.transform.position.x)));
 	}
+
+    protected virtual void SetValueInHeavy(float value)
+    {
+        Debug.LogWarning("SetValueInHeavy Not implemented");
+    }
+
+
 }
